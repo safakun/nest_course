314b9a729919e726d971aee92864329d51c41589
safakun <safarovdeem@gmail.com>
2023-12-16 09:12:43 +0500

    added login jwt function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
@@ -12,7 +12,8 @@ export class AuthService {
         private jwtService: JwtService) {}
    
     async login(userDto: CreateUserDto) {
-       
+        const user = await this.validateUser(userDto);
+        return this.generateToken(user);
     }
 
    
@@ -30,11 +31,21 @@ export class AuthService {
        return this.generateToken(user);
 }
 
-async generateToken(user: User) {
+private async generateToken(user: User) {
     const payload = {email: user.email, id: user.id, roles: user.roles};
     return {
         token: this.jwtService.sign(payload)
     }
+}
+
+private async validateUser(userDto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(userDto.email);
+    const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+    if (user && passwordEquals) {
+        return user;
+    }
+
+    throw new UnauthorizedException({message: "Password or email are not correct"});
 }
 
 }
