5686e647d08d946bbf3eaf2531116907ea888b6e
safakun <safarovdeem@gmail.com>
2023-12-16 17:11:02 +0500

    added post add func

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post, UploadedFile } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostsService } from './posts.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('posts')
 export class PostsController {
@@ -8,9 +9,10 @@ export class PostsController {
     constructor(private postService: PostsService) {}
     
     @Post()
+    @UseInterceptors(FileInterceptor('image'))
     createPost(@Body() dto: CreatePostDto,
     @UploadedFile() image
     ) {
-        this.postService.create(dto, image)
+        return this.postService.create(dto, image)
     }
 }
